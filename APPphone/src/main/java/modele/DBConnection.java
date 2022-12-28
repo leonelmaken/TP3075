745c3900d98d66e36af7f23da1f5739d7ff82a22
links <nduwarugirabruno@gmail.com>
2022-12-28 20:40:32 +0100

    ResponsableCoin & Database

---
+++ APPphone/src/main/java/modele/DBConnection.java
@@ -0,0 +1,48 @@
+package modele;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private final String dbURL = "jdbc:mysql://localhost:3306/MoBileDao";
+    private final String userName = "root";
+    private final String password = "";
+    private Connection connection;
+
+    public DBConnection() {
+        System.out.println("\n          Connexion à la base de donnée...");
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(dbURL, userName, password);
+            if (connection!=null) System.out.println("          Connexion établie.\n");
+            else System.out.println("          Connexion échouée.\n");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        if (connection!=null)
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    @Override
+    public String toString() {
+        return "DBConnection:" +
+                "\n\tURL = " + dbURL +
+                "\n\tUserName = " + userName +
+                "\n\tPassword = " + password +
+                "\n\tConnection = " + connection;
+    }
+
+}
--- AppPhone.sql
+++ AppPhone.sql
@@ -1,3 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 28 déc. 2022 à 12:31
+-- Version du serveur : 10.4.20-MariaDB
+-- Version de PHP : 8.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 -- ------------------------------------------------------------------------------
 -- - Reconstruction de la base de données                                     ---
 -- ------------------------------------------------------------------------------
@@ -5,82 +24,201 @@ DROP DATABASE IF EXISTS MoBileDao;
 CREATE DATABASE MoBileDao;
 USE MoBileDao;
 
-CREATE TABLE Compte (
-	Numero				int(20)		PRIMARY KEY,
-	nameUser				varchar(20)	REFERENCES nameUser(User),
-	Solde		int(600),
-) ENGINE = InnoDB;
-INSERT INTO Compte (Numero, nameUser,Solde) VALUES ( 653241315, 'Anderson',75000 );
-INSERT INTO  Compte (Numero, nameUser,Solde)VALUES ( 653124578, 'Skywalker',0 );
-INSERT INTO Compte (Numero, nameUser,Solde) VALUES ( 658234479, 'Elena',75000 );
-INSERT INTO Compte (Numero, nameUser,Solde)VALUES ( 699882231, 'Klaus',500000 );
-INSERT INTO  Compte (Numero, nameUser,Solde)VALUES ( 679285364, 'Bonnie',900000);
-INSERT INTO  Compte (Numero, nameUser,Solde)VALUES ( 677951025,'CamWater',6000000);
-INSERT INTO  Compte (Numero, nameUser,Solde)VALUES ( 652584617 ,'Eneo',5000000);
--- -----------------------------------------------------------------------------
--- - Construction de la table des utilisateurs                               ---
--- -----------------------------------------------------------------------------
-CREATE TABLE Users (
-	IdUser				int(4)		PRIMARY KEY AUTO_INCREMENT,
-	nameUser				varchar(20)	NOT NULL,
-	CodePin			int(20)	NOT NULL,
-	Numero	int(20)		NOT NULL REFERENCES Numero(Compte),
-	Solde               int(255)   REFERENCES Compte(Solde)
-) ENGINE = InnoDB;
-
-
-INSERT INTO Users (IdUser, nameUser, CodePin,Numero,Solde) VALUES ( 1, 'Anderson',15780,653124707,75000 );
-INSERT INTO  Users (IdUser, nameUser, CodePin,Numero,Solde)VALUES ( 2, 'Skywalker',131500,612034578,0 );
-INSERT INTO  Users (IdUser, nameUser, CodePin,Numero,Solde)VALUES ( 3, 'Elena',15893,658791423,85200);
-INSERT INTO  Users (IdUser, nameUser, CodePin,Numero,Solde)VALUES ( 4, 'Klaus',251346,694782513,50000 );
-INSERT INTO Users (IdUser, nameUser, CodePin,Numero,Solde) VALUES ( 5, 'Bonnie',68932,678123547,850000);
-INSERT INTO  Users (IdUser, nameUser, CodePin,Numero,Solde)VALUES ( 6,'CamWater',58963,677951025,6000000);
-INSERT INTO  Users (IdUser, nameUser, CodePin,Numero,Solde)VALUES ( 7,'Eneo',89632,652584617,5000000);
-
-SELECT * FROM Users;
-
--- -----------------------------------------------------------------------------
--- - Construction de la table des administrateurs                            ---
--- -----------------------------------------------------------------------------
-
 CREATE TABLE Admins (
-	IdUser				int(4)		NOT NULL REFERENCES Users(IdUser),
-	CodePin				int(4)		NOT NULL REFERENCES Users(CodePin),
-	nameUser		    varchar(20)		NOT NULL REFERENCES Users(nameUser),
-	Numero              int(20)     NOT NULL REFERENCES Users(Numero),
-	Solde               int(255)   REFERENCES Compte(Solde)
-) ENGINE = InnoDB;
+  idAdmin int(4) NOT NULL,
+  `CodePin` int(25) NOT NULL,
+  `nameUser` varchar(255) NOT NULL,
+  `Numero` int(25) NOT NULL,
+  `Solde` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO Admins (IdUser, CodePin,nameUser,Numero,Solde)VALUES ( 1,15780, 'Anderson',653124707,75000);
-INSERT INTO Admins (IdUser, CodePin,nameUser,Numero,Solde) VALUES ( 5,68932, 'Bonnie',678123547,850000 );
+--
+-- Déchargement des données de la table `Admins`
+--
 
--- -----------------------------------------------------------------------------
--- - Construction de la tables des ComptStructures en vente                         ---
--- -----------------------------------------------------------------------------
-CREATE TABLE ComptStructure (
-	IdCompt			int(4)		PRIMARY KEY AUTO_INCREMENT,
-	nameUser		VARCHAR(200)     	NOT NULL REFERENCES Users(nameUser),
-	Solde				int(255)       REFERENCES Compte(Solde),
-	Numero		int(20)	NOT NULL REFERENCES Compte(Numero)
-) ENGINE = InnoDB;
+INSERT INTO Admins (idAdmin, `CodePin`, `nameUser`, `Numero`, `Solde`)VALUES
+(1, 1245, 'arnaud', 6546545, 250000),
+(2, 1234, 'bobi', 456987, 40000);
 
-INSERT INTO ComptStructure (IdCompt, nameUser,Solde,Numero ) VALUES ( 1,'Eneo',5000000,652584617 );
-INSERT INTO ComptStructure ( IdCompt, nameUser,Solde,Numero ) VALUES ( 2,'CamWater',6000000,677951025);
+-- --------------------------------------------------------
 
-SELECT * FROM ComptStructure;
+--
+-- Structure de la table `Compte`
+--
 
--- -----------------------------------------------------------------------------
--- - Construction de la tables des Responsables de Coin de ventes                                 ---
--- -----------------------------------------------------------------------------
+CREATE TABLE Compte (
+  idCompte int(20) NOT NULL,
+  `nameUser` varchar(25) NOT NULL,
+  `Solde` int(255) NOT NULL,
+  `Numero` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Compte`
+--
+
+INSERT INTO Compte (idCompte, `nameUser`, `Solde`, `Numero`)VALUES
+(1, 'Anderson', 250000, 620151842),
+(2, 'Skywalker', 40000, 679707142),
+(3, 'Elena', 45000, 695156866),
+(4, 'Klaus', 50000, 620175731),
+(5, 'Bonnie', 900000, 679285364),
+(6, 'CamWater', 6000000, 677951025),
+(7, 'Eneo', 5000000, 652584617);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `CompteStructure`
+--
+
+CREATE TABLE CompteStructure (
+  idStructure int(11) NOT NULL,
+  `nameUser` varchar(255) NOT NULL,
+  `Solde` int(255) NOT NULL,
+  `Numero` int(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `CompteStructure`
+--
+
+INSERT INTO CompteStructure (idStructure, `nameUser`, `Solde`, `Numero`)VALUES
+(3, 'tankeu', 45000, 44444),
+(4, 'cynthia', 50000, 22222);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ResponsCoin`
+--
+
 CREATE TABLE ResponsCoin (
-	IdUser		int(4)		NOT NULL REFERENCES Users(IdUser),
-	Poste			VARCHAR(200)		NOT NULL,
-	Numero       int(20)  NOT NULL REFERENCES Compte(Numero),
-	Solde        int(255)    REFERENCES Compte(Solde)
-) ENGINE = InnoDB;
+  idResp int(4) NOT NULL,
+  `nameUser` varchar(255) NOT NULL,
+  `Numero` int(25) NOT NULL,
+  `Solde` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO ResponsCoin (IdUser,Poste,Numero,Solde) VALUES (2,"CalBoxeur",612034578,0); 
-INSERT INTO ResponsCoin (IdUser,Poste,Numero,Solde) VALUES (3,"CalBoxeur",658791423,85200); 
-INSERT INTO ResponsCoin (IdUser,Poste,Numero,Solde) VALUES (4,"CalBoxeur",694782513,50000); 
+--
+-- Déchargement des données de la table `ResponsCoin`
+--
 
+INSERT INTO ResponsCoin (idResp, `nameUser`, `Numero`, `Solde`)VALUES
+(2, 'bobi', 456987, 40000),
+(3, 'tankeu', 154565, 45000);
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Users`
+--
+
+CREATE TABLE Users (
+  `idUser` int(2) NOT NULL,
+  `idStructure` int(4) NOT NULL,
+  `idAdmin` int(4) NOT NULL,
+  `idResp` int(4) NOT NULL,
+  `idCompte` int(4) NOT NULL,
+  `nameUser` varchar(200) NOT NULL,
+  `CodePin` int(8) NOT NULL,
+  `Numero` int(12) NOT NULL,
+  `Solde` int(255) NOT NULL,
+  `NumeroCni` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `Users`
+--
+
+INSERT INTO Users (idUser, idStructure, idAdmin, idResp, idCompte, `nameUser`, `CodePin`, `Numero`, `Solde`, `NumeroCni`) VALUES
+(1, 1, 1, 1, 1, 'arnaud', 1245, 45456423, 250000, 0),
+(3, 2, 2, 2, 2, 'bobi', 1234, 456987, 40000, 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Admins`
+--
+ALTER TABLE Admins
+  ADD PRIMARY KEY (idAdmin);
+
+--
+-- Index pour la table `Compte`
+--
+ALTER TABLE Compte
+  ADD PRIMARY KEY (idCompte);
+
+--
+-- Index pour la table `CompteStructure`
+--
+ALTER TABLE CompteStructure
+  ADD PRIMARY KEY (idStructure);
+
+--
+-- Index pour la table `ResponsCoin`
+--
+ALTER TABLE ResponsCoin
+  ADD PRIMARY KEY (idResp);
+
+--
+-- Index pour la table `Users`
+--
+ALTER TABLE Users
+  ADD PRIMARY KEY (idUser),
+  ADD UNIQUE KEY `structure` (idStructure),
+  ADD UNIQUE KEY `admin` (idAdmin),
+  ADD UNIQUE KEY `responsable` (idResp),
+  ADD UNIQUE KEY `compte` (idCompte);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Admins`
+--
+ALTER TABLE Admins
+  MODIFY idAdmin int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `Compte`
+--
+ALTER TABLE Compte
+  MODIFY idCompte int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `CompteStructure`
+--
+ALTER TABLE CompteStructure
+  MODIFY idStructure int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `ResponsCoin`
+--
+ALTER TABLE ResponsCoin
+  MODIFY idResp int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `Users`
+--
+ALTER TABLE Users
+  MODIFY idUser int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `Users`
+--
+ALTER TABLE Users
+  ADD CONSTRAINT `if_admin_user` FOREIGN KEY (idAdmin) REFERENCES Admins (idAdmin) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `if_compte_user` FOREIGN KEY (idCompte) REFERENCES Compte (idCompte) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
